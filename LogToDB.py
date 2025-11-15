71ce64c65ccaa94c352174430fdadb2921d1f8ff
Yasuotem <yso19669Tera@gmail.com>
2025-11-15 16:30:46 +0900

    CSV読み込みテスト

--- LogToDB.py
+++ LogToDB.py
@@ -3,10 +3,11 @@ import duckdb
 import yaml
 import os
 
+#YAMLのデータを読み込み
 with open("config.yaml", "r", encoding="utf-8") as f:
     config = yaml.safe_load(f)
-
 database = config["database"]
+tables = config["tables"]
 
 def csv_to_db(filepath, table, skip):
     df = pd.read_csv(filepath, skiprows=skip)
@@ -29,11 +30,15 @@ def csv_to_db(filepath, table, skip):
         con.execute(f"CREATE TABLE IF NOT EXISTS {table} AS SELECT * FROM df LIMIT 0")
         #テーブルにレコード追加
         con.execute(f"INSERT INTO {table} SELECT * FROM df")
+        print(f"{filename}を追加しました")
     elif exists == False:pass #ファイルが登録されているが、完了していない場合
-    elif exists == True:pass #ファイルが登録されていて完了している場合
+    elif exists == True: #ファイルが登録されていて完了している場合
+        print(f"{filename}はDB内に存在しています")
 
 
 import argparse
+import fnmatch
+
 def main():
     parser = argparse.ArgumentParser(description="CSVファイルをDBに登録します")
     parser.add_argument("files", nargs="+", help="処理するCSVファイル")
@@ -41,8 +46,15 @@ def main():
     args = parser.parse_args()
     files = args.files
 
-    print(files)
-    input("test")
+    for table_data in tables:
+        pattern = table_data["pattern"]
+        table = table_data["table"]
+        skip = table_data["skip"]
+
+        for file in fnmatch.filter(files, f"*{pattern}"):
+            csv_to_db(file, table, skip)
+
+    input("処理が終了しました。")
 
 
 if __name__ == "__main__":
