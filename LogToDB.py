45af16e3aece919632d4944373f54dea2a64d908
Yasuotem <yso19669Tera@gmail.com>
2025-11-15 17:15:02 +0900

    CSV読み込みバグ修正

--- LogToDB.py
+++ LogToDB.py
@@ -19,21 +19,23 @@ def csv_to_db(filepath, table, skip):
 
     #ファイル名管理テーブルの定義
     con.execute("""CREATE TABLE IF NOT EXISTS files (
-                source VERCHAR,
-                compete BOOL)""")
+                source VARCHAR,
+                complete BOOL)""")
     #ファイルの登録データを探す
-    exists = con.execute(f"SELECT complete FROM files WHERE source = {filename}").fetchone()[0]
+    exists = con.execute("SELECT complete FROM files WHERE source = ?", [filename]).fetchone()
     if exists is None: #ファイルが未登録の場合
         #ファイル登録
-        con.execute(f"INSERT INTO files VALUES ({filename}, {True})")
+        con.execute("INSERT INTO files VALUES (?, ?)", [filename, True])
         # テーブルがなければ作成（型は df から推論）
         con.execute(f"CREATE TABLE IF NOT EXISTS {table} AS SELECT * FROM df LIMIT 0")
         #テーブルにレコード追加
         con.execute(f"INSERT INTO {table} SELECT * FROM df")
         print(f"{filename}を追加しました")
-    elif exists == False:pass #ファイルが登録されているが、完了していない場合
-    elif exists == True: #ファイルが登録されていて完了している場合
-        print(f"{filename}はDB内に存在しています")
+    else:
+        complete = exists[0]
+        if complete == False:pass #ファイルが登録されているが、完了していない場合
+        elif complete == True: #ファイルが登録されていて完了している場合
+            print(f"{filename}はDB内に存在しています")
 
 
 import argparse
@@ -46,12 +48,18 @@ def main():
     args = parser.parse_args()
     files = args.files
 
+    if files is None:
+        input("ファイルを選択してください")
+        return
+
     for table_data in tables:
         pattern = table_data["pattern"]
         table = table_data["table"]
-        skip = table_data["skip"]
+        skip = table_data["skip_rows"]
+        print(pattern)
 
         for file in fnmatch.filter(files, f"*{pattern}"):
+            print(file)
             csv_to_db(file, table, skip)
 
     input("処理が終了しました。")
