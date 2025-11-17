9ea6bae009b94fa6fe1c7afacad1b37a1f3486dd
Yasuotem <yso1969Tera@gmail.com>
2025-11-17 10:09:42 +0900

    分岐をmatchで整理

--- LogToDB.py
+++ LogToDB.py
@@ -3,11 +3,10 @@ import duckdb
 import yaml
 import os
 import fnmatch
-from datetime import datetime, time
+from datetime import datetime
 import argparse
 from pathlib import Path
 import zipfile
-from dataclasses import dataclass
 from functools import partial
 from typing import TypedDict
 import dateutil
@@ -24,13 +23,6 @@ class Config(TypedDict):
     database: str                    # DuckDB データベースファイル名
     tables: list[TableConfig]        # 複数テーブルの設定リスト
 
-# Options dataclass
-@dataclass(frozen=True)
-class Options:
-    con: duckdb.DuckDBPyConnection
-    config: Config
-    files: list[Path]
-
 #CSVをdfに変換してテーブルに追加, filesテーブルにファイル名を登録
 def csv_to_db(con, file, table: TableConfig, filename, last_modded):
     #csvのデータをdfに読み込む
@@ -44,43 +36,41 @@ def csv_to_db(con, file, table: TableConfig, filename, last_modded):
     #ファイル更新日時からログが完了しているか判別する
     complete = last_modded.time() >= dateutil.parser.parse(table["log_completion_time"]).time()
 
-    #ファイル名管理テーブルの定義
-    con.execute("""CREATE TABLE IF NOT EXISTS files (
-                source VARCHAR,
-                complete BOOL)""")
     table_name = table["table_name"]
     #ファイルの登録データを探す
-    exists = con.execute("SELECT complete FROM files WHERE source = ?", [filename]).fetchone()
-    if exists is None: #ファイルが未登録の場合
-        # テーブルがなければ作成（型は df から推論）
-        con.execute(f"CREATE TABLE IF NOT EXISTS {table_name} AS SELECT * FROM df LIMIT 0")
-        #テーブルにレコード追加
-        con.execute(f"INSERT INTO {table_name} SELECT * FROM df")
-        #ファイル登録
-        con.execute("INSERT INTO files VALUES (?, ?)", [filename, complete])
-        print(f"{filename}を追加しました")
-    else:
-        is_complete = exists[0]
-        if is_complete == False: #ファイルが登録されているが、完了していない場合
+    match con.execute("SELECT complete FROM files WHERE source = ?", [filename]).fetchone():
+        case None: #ファイルが未登録の場合
+            # テーブルがなければ作成（型は df から推論）
+            con.execute(f"CREATE TABLE IF NOT EXISTS {table_name} AS SELECT * FROM df LIMIT 0")
+            #ファイル登録
+            con.execute("INSERT INTO files VALUES (?, FALSE)", [filename])
+            #テーブルにレコード追加
+            con.execute(f"INSERT INTO {table_name} SELECT * FROM df")
+            #ファイル登録データを更新する
+            con.execute("UPDATE files SET complete = ? WHERE source = ?", [complete, filename])
+            print(f"{filename}を追加しました")
+
+        case (False,): # ファイルが登録されているが未完了の場合
             #テーブル内にある同ファイルのデータを消して入れなおす
             con.execute(f"DELETE FROM {table_name} WHERE source = ?", [filename])
             con.execute(f"INSERT INTO {table_name} SELECT * FROM df")
             #ファイル登録データを更新する
             con.execute("UPDATE files SET complete = ? WHERE source = ?", [complete, filename])
             print(f"{filename}を更新しました") 
-        elif is_complete == True: #ファイルが登録されていて完了している場合
+            
+        case (True,): #ファイルが登録されていて完了している場合
             print(f"{filename}はDB内に存在しています")
 
 #ファイルをcsv/csvの入ったzipで分類して処理
-def read_files(opt: Options):
+def read_files(con, config, files):
     #yamlファイルからパターンの一致するtableを取得する
     def detect_pattern(str):
-        for table in opt.config["tables"]:
+        for table in config["tables"]:
             if fnmatch.fnmatch(str, table["pattern"]):
                 return table
         return None
-
-    for filepath in opt.files:
+    
+    for filepath in files:
         match filepath.suffix:
             case '.csv':
                 filename = filepath.name
@@ -90,7 +80,7 @@ def read_files(opt: Options):
                     ts = os.path.getmtime(filepath)
                     last_modded = datetime.fromtimestamp(ts)
                     
-                    csv_to_db(opt.con, filepath, table, filename, last_modded)
+                    csv_to_db(con, filepath, table, filename, last_modded)
             case '.zip':
                 with zipfile.ZipFile(filepath) as z:
                     print(filepath.name)
@@ -103,7 +93,7 @@ def read_files(opt: Options):
                                 #最終更新日時をdatetimeで取得
                                 last_modded = datetime(*info.date_time)
 
-                                csv_to_db(opt.con, z.open(info), table, filename, last_modded)
+                                csv_to_db(con, z.open(info), table, filename, last_modded)
             case _:
                 pass
 
@@ -130,8 +120,12 @@ def main():
         input()
         return
     
-    opt = Options(con, config, args.files)
-    read_files(opt)
+    #ファイル名管理テーブルの定義
+    con.execute("""CREATE TABLE IF NOT EXISTS files (
+                source VARCHAR,
+                complete BOOL)""")
+  
+    read_files(con, config, args.files)
     
     print("処理が終了しました。")
     input()
